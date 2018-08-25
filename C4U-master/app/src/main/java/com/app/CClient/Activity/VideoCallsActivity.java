500041e41b27ea0f0753cbe2af371b8f94f8b1e9
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-08-25 17:11:24 +0800

    Most of the UI view is completed

---
+++ C4U-master/app/src/main/java/com/app/CClient/Activity/VideoCallsActivity.java
@@ -0,0 +1,15 @@
+package com.app.CClient.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.app.CClient.fragmenttabhost.R;
+
+public class VideoCallsActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitycalls);
+    }
+}
