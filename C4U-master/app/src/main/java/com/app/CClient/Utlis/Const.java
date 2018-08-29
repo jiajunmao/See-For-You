01a674c92c0a8d7d428e1364a6294d873b7c7069
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-08-29 19:46:14 +0800

    Supplementary code

--- C4U-master/app/src/main/java/com/app/CClient/Utlis/Const.java
+++ C4U-master/app/src/main/java/com/app/CClient/Utlis/Const.java
@@ -8,4 +8,6 @@ public class Const {
     public static double LONGITUDE = 0;//经度
     public static double LATITUDE = 0;//纬度
     public static String ADDRESS = "";//地址
+    public static int add = 0;
+    public  static int msg = 0;
 }
