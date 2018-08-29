94ba8bd0efef19e1def107b31f9767a7c601be66
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-08-29 20:38:20 +0800

    Complete String function

---
+++ C4U-master/app/src/main/java/com/app/CClient/Utlis/StringUtil.java
@@ -0,0 +1,45 @@
+package com.app.CClient.Utlis;
+
+import android.util.Log;
+
+public class StringUtil {
+	
+	private static final String TAG = "SubstringUtil";
+	/**
+	 * remove ssid \"
+	 * @param ssid
+	 * @return
+	 */
+	public static String getSSID(String ssid){
+		if(ssid.length()>=2){
+			Log.e(TAG, ssid.substring(ssid.length()-1,ssid.length()));
+			Log.d(TAG, ssid.substring(0, 1));
+			if(ssid.substring(0,1).equals("\"")&&ssid.substring(ssid.length()-1,ssid.length()).equals("\"")){
+				Log.d(TAG, "substring:"+ssid);
+				ssid= ssid.substring(1, ssid.length()-1);
+			}	
+			Log.i(TAG, ssid);
+		}
+		return ssid;
+	}
+	/**
+	 *  add ssid \"
+	 * @param ssid
+	 * @return
+	 */
+	public static String setSSID(String ssid){
+		if(ssid.length()>=2){
+			Log.e(TAG, ssid.substring(ssid.length()-1,ssid.length()));
+			Log.d(TAG, ssid.substring(0, 1));
+			if(!ssid.substring(0,1).equals("\"")&&!ssid.substring(ssid.length()-1,ssid.length()).equals("\"")){
+				Log.d(TAG, "substring:"+ssid);
+				ssid=new String("\""+ssid+"\"");
+			}	
+			Log.i(TAG, ssid);
+		}else{
+			ssid=new String("\""+ssid+"\"");
+		}
+		return ssid;
+	}
+	
+}
