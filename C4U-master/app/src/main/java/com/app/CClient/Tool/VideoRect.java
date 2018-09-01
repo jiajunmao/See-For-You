d8c5fdf66af492adc40b63e4220f2643f0e8f410
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-09-02 01:05:54 +0800

    Complete video function

---
+++ C4U-master/app/src/main/java/com/app/CClient/Tool/VideoRect.java
@@ -0,0 +1,23 @@
+package com.app.CClient.Tool;
+
+import android.graphics.RectF;
+
+public class VideoRect {
+    public RectF rect;
+
+    public int startH;
+    public int startMin;
+    public int startSec;
+    public int stopH;
+    public int stopMin;
+    public int stopSec;
+
+    public VideoRect(int startH, int startMin, int startSec, int stopH, int stopMin, int stopSec) {
+        this.startH = startH;
+        this.startMin = startMin;
+        this.startSec = startSec;
+        this.stopH = stopH;
+        this.stopMin = stopMin;
+        this.stopSec = stopSec;
+    }
+}
