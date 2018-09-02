824d8e5230a6047a59c8e9cae1f69aef720bb4ba
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-09-03 02:44:08 +0800

    Add to username

--- C4U-master/app/src/main/java/com/app/CClient/Tool/UserRole.java
+++ C4U-master/app/src/main/java/com/app/CClient/Tool/UserRole.java
@@ -13,6 +13,15 @@ import com.app.CClient.fragmenttabhost.R;
     public int nameResId;
     public byte type;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String username;
     UserRole(byte type, int nameResId){
         this.nameResId = nameResId;
         this.type = type;
