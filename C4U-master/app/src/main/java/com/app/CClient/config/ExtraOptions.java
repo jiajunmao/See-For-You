b661458d427c56867e25aefa2d157e84b60acd1b
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-09-04 00:33:48 +0800

    Add data and delete data function.

--- C4U-master/app/src/main/java/com/app/CClient/config/ExtraOptions.java
+++ C4U-master/app/src/main/java/com/app/CClient/config/ExtraOptions.java
@@ -8,4 +8,10 @@ public class ExtraOptions {
     public static void provide() {
         /// EMPTY NOW
    }
+   public static void adddata(){
+
+   }
+   public static void removedata(){
+
+   }
 }
