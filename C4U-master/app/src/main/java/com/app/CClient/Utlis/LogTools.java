9ff0fd7a00cedd0250c8a66c54dce6f4cb9940e2
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-09-09 14:02:33 +0800

    Remove invalid log code

--- C4U-master/app/src/main/java/com/app/CClient/Utlis/LogTools.java
+++ C4U-master/app/src/main/java/com/app/CClient/Utlis/LogTools.java
@@ -69,6 +69,5 @@ public class LogTools {
         Log.e(TAG, "==================================");
         Log.e(TAG, msg);
         Log.e(TAG, stackTrace);
-        Log.e(TAG, "-----------------------------------");
     }
 }
