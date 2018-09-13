49c350b0fa60d5182ef32ad88ae9a99a65dbf01e
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-09-13 20:53:12 +0800

    Modify server function

--- C4U-master/app/src/main/java/com/app/CClient/config/DemoServers.java
+++ C4U-master/app/src/main/java/com/app/CClient/config/DemoServers.java
@@ -1,10 +1,6 @@
 package com.app.CClient.config;
 
 public class DemoServers {
-
-    //
-    // 好友列表信息服务器地址
-    //
     private static final String API_SERVER_TEST = "https://apptest.netease.im/api/"; // 测试
     private static final String API_SERVER = "https://app.netease.im/api/"; // 线上
 
