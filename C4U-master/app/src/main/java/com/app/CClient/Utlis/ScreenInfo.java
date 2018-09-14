bf8b72bfaee13b9334e924dbb507792d849a7aa1
tao168jy123 <3096187479@qq.com>
2018-09-15 00:51:38 +0800

    Complete unit conversion function

--- C4U-master/app/src/main/java/com/app/CClient/Utlis/ScreenInfo.java
+++ C4U-master/app/src/main/java/com/app/CClient/Utlis/ScreenInfo.java
@@ -10,13 +10,11 @@ public class ScreenInfo {
     public static int screen_w;
     public static int screen_h;
     public static float screen_d;
-
     public static void initData(Activity activity) {
         DisplayMetrics metrics = new DisplayMetrics();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
             activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
         } else {
-            // TODO this is not correct, but we don'thumb_black really care pre-kitkat
             activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         }
         screen_w = metrics.widthPixels; // 得到屏幕宽度
@@ -24,12 +22,8 @@ public class ScreenInfo {
         screen_d = metrics.density;
         Log.e("requestWindowFeature", "initData:");
     }
-
     /**
      * 单位转换方法
-     *
-     * @param dp
-     * @return
      */
     public static int Dp2Px(float dp) {
         return (int) (dp * screen_d + 0.5f);
