1a5f162079fba4ada98f1310600777ab8cee3e42
tao168jy123 <3096187479@qq.com>
2018-09-16 18:59:12 +0800

    modification VisuallyimpairedActivity

--- C4U-master/app/src/main/java/com/app/CClient/Activity/VisuallyimpairedActivity.java
+++ C4U-master/app/src/main/java/com/app/CClient/Activity/VisuallyimpairedActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import com.app.CClient.fragmenttabhost.R;
